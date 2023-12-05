1769e7185f51c2fffa75b6be805e220cc1b736fe
ManuelGurbanov <manuelgurbanov@gmail.com>
2023-12-05 01:48:36 -0300

    Lista de jugadores en pantalla
    Ya se muestran los jugadores en pantalla, sin css y provisorio

--- arsenal/views.py
+++ arsenal/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
+from app_socios.models import Jugador  # Asegúrate de importar tu modelo Jugador
 class InicioView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['jugadors'] = Jugador.objects.all()
+        return context
