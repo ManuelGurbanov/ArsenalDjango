f4e2a2c09308f071a4fb184c7b96f1239351d2f7
Nicolas Montagut <nico.monta99@gmail.com>
2023-12-08 18:37:31 -0300

    cambios para delpoy

--- arsenal/wsgi.py
+++ arsenal/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for arsenal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# +++++++++++ DJANGO +++++++++++
 import os
+import sys
+
+# le indicamos el path del project
+path = '/home/montagut1/ArsenalDjango/arsenal'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-
+# le indicamos el lugar de los settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arsenal.settings')
-
 application = get_wsgi_application()
