ece3075960b0b86c719f60c2fd0791ce8a53c4d3
ManuelGurbanov <manuelgurbanov@gmail.com>
2023-12-08 17:01:20 -0300

    Cambios en el front

--- arsenal/views.py
+++ arsenal/views.py
@@ -1,10 +1,11 @@
 from django.views.generic import TemplateView
 from app_socios.models import Jugador
+from django.shortcuts import render
 
 class InicioView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['jugadors'] = Jugador.objects.all()
+        context['jugadores'] = Jugador.objects.all()
         return context
