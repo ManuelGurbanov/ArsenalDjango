4bfdfd3c9c79f757ec2fefc0218c575a2e25af96
Nicolas Montagut <nico.monta99@gmail.com>
2023-12-08 21:21:45 -0300

    revierto cambios en wsgi

--- arsenal/wsgi.py
+++ arsenal/wsgi.py
@@ -1,13 +1,7 @@
 # +++++++++++ DJANGO +++++++++++
 import os
-import sys
-
-# le indicamos el path del project
-path = '/home/montagut1/ArsenalDjango'
-if path not in sys.path:
-    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-# le indicamos el lugar de los settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arsenal.settings')
 application = get_wsgi_application()
